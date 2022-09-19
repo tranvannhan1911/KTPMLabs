4fcb25964d62ac38ff86dfc97f6fb71550d2866f
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-09-19 11:50:57 +0700

    entity

--- Tuan4/datajpa/src/main/java/com/example/springdatajpa/datajpa/entity/ChuyenBay.java
+++ Tuan4/datajpa/src/main/java/com/example/springdatajpa/datajpa/entity/ChuyenBay.java
@@ -1,10 +1,25 @@
 import java.sql.Date;
 
+@Entity
+@Table(name="ChuyenBay")
 public class ChuyenBay {
+    @Id
+    @GeneratedValue
+    @Column(name="MaCB")
     private long maCB;
+    
+    @Column(name="GaDi")
     private String gaDi;
+
+    @Column(name="GaDen")
     private String gaDen;
+    
+    @Column(name="DoDai")
     private float doDai;
+    
+    @Column(name="GioDi")
     private Date gioDi;
+    
+    @Column(name="GioDen")
     private Date gioDen;
 }
