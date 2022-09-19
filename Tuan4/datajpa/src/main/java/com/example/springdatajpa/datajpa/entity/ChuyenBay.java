c149c265af983f350d1e64fd879a0c8a1f241de2
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-09-19 11:44:12 +0700

    tuan 4

---
+++ Tuan4/datajpa/src/main/java/com/example/springdatajpa/datajpa/entity/ChuyenBay.java
@@ -0,0 +1,10 @@
+import java.sql.Date;
+
+public class ChuyenBay {
+    private long maCB;
+    private String gaDi;
+    private String gaDen;
+    private float doDai;
+    private Date gioDi;
+    private Date gioDen;
+}
